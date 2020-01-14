5c7307b3112a331c529e370fa96b361f9c3d3b3d
devgamble <dgamble@ucsb.edu>
2020-01-14 12:08:45 -0800

    Project directory rearrangement, Created script for cleaning  cch2 occurrence data

---
+++ data_cleaning/CCH2_scripts_data/cch2_cleaning.R
@@ -0,0 +1,11 @@
+#
+# Data Cleaning for the (almost) raw CCH2 occurrence dataset
+#
+
+#Load Packages
+library(stringr)
+library(magrittr)
+library(tidyverse)
+library(ggplot2)
+library(here)
+
--- data_cleaning/image_dl.R
+++ data_cleaning/image_dl.R
@@ -4,8 +4,7 @@
 #
 ###########
 
-# Devin Gamble
-# Dec 20, 2019
+# Devin Gamble, Dec 20 2019
 
 #Load Packages
 library(tidyverse)
@@ -13,23 +12,21 @@ library(here)
 library(beepr)
 
 
-images_df <- read_csv(here("data_cleaning", "images_cch2_Dec19.csv"))
+images_df <- read_csv(here("data_cleaning", "CCH2_scripts_data", "images_cch2_Dec19.csv"))
 
 
-#Add specimen_nummber to images_df
-id_spec <- read_csv(here("data_cleaning", "nemo_cch2_Dec2019.csv")) %>% 
+#Add specimen_number from full dataset to images_df
+id_spec <- read_csv(here("data_cleaning", "CCH2_scripts_data", "nemo_cch2_Dec2019.csv")) %>% 
   select(id, specimen_number)
 
-images_df <- merge(id_spec, images_df)
+images_df <- merge(id_spec, images_df) #merges by `id`
 
 
 #Testing with one image:
 download.file(images_df[1,3], destfile = here("data_cleaning", "cch2_images", paste(images_df[1,1], ".jpg", sep = "")), method = "auto", mode = "wb")
 
-#Here I've set the filenames to be the `id` column, but maybe `specimen_number` would be better?
-#id likely to differ (?) between cch1 and cch2 datasets
-
-#Note: It may be more appropriate (and less risky) to download images to a folder outside the R project this script was saved in. This will avoid complications with commiting/pushing to Github.
+#Here I've set the filenames to be from the `id` column, but maybe `specimen_number` would be better?
+#id might differ (?) between cch1 and cch2 datasets
 
 
 #Trying 10 before going big...
@@ -42,16 +39,14 @@ for(i in 1:10){
 # WARNING!
 ######
 #
-# DO NOT
-# run the following code 
-# UNLESS 
-# you are prepared to wait HOURS to download these all of these images AND you have sufficient space on your local machine (Roughly 4.7 Gigabytes)
+# DO NOT run the following code UNLESS 
+# you are prepared to wait hours to download all of these images AND you have sufficient space on your local machine (Roughly 4.7 Gigabytes)
 
 # Also
-# I would suggest un-staging these images before you commit/push to github, if you choose to download these images inside the R project folder as I have done here.
+# I would suggest un-staging these images before you commit/push to github. Alternatively, specify a gitignore file to untrack the image folder.
 
 
-#SAVE ALL IMAGES:
+#Save all images:
 for(i in 1:length(images_df[,3])){
   download.file(images_df[i,3], destfile = here("data_cleaning", "cch2_images", paste(images_df[i,1], ".jpg", sep = "")), method = "auto", mode = "wb")
 }
