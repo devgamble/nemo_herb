429154780f442c6d127dd3a0cb2ac3bee6a8f825
devgamble <dgamble@ucsb.edu>
2020-02-28 09:26:17 -0800

    nemo_cch2_cleaned_V2 with dupes removed, datum & URLs retained; re-coded split csvs for georeferencing

--- data_cleaning/CCH2_scripts_data/cch2_cleaning.R
+++ data_cleaning/CCH2_scripts_data/cch2_cleaning.R
@@ -16,12 +16,14 @@ library(lubridate)
 
 # NOTE: The csv I've read in here was trimmed a bit (irrelevant columns removed) but this workflow should work just as well with a csv downloaded straight from CCH2.
 
+# NOTE 2: I chosen to create an updated cleaned csv in this script so I don't have to pull from the original occurrence data again. Here I remove duplicates and add back two columns. the first csv `nemo_cch1_cleaned.csv` should functionally be the same
+
 # Clean & Standardize Dates
 
 nemo_cch2 <- read_csv(here::here("data_cleaning", "CCH2_scripts_data", "nemo_cch2_Dec2019.csv")) %>% 
   filter(!day == 0) %>% #remove obs where day = 0 or is blank
   mutate(date_new = make_date(year = year, month = month, day = day)) %>%  #Combine YYYY MM DD
-  select(- c(institutionCode, recordId, references)) %>% 
+  select(- c(institutionCode, recordId)) %>% 
   mutate(sub_sp = case_when(taxonID == 205100 | taxonID == 205103 ~ "menziesii",
                             taxonID == 205101 ~ "atomaria",
                             taxonID == 205102 | taxonID == 210481 |taxonID == 218999 | scientificName %in% c("Nemophila menziesii var. integrefolia", "Nemophila menziesii var. incana", "Nemophila menziesii subsp. australis") ~ "integrifolia")) %>% 
@@ -45,7 +47,8 @@ nemo_2 <- nemo_cch2 %>%
 # `minimumElevation` used for ranges and when only one measure of elevation present
 
 nemo_2 <- nemo_2 %>% 
-  rename(elev_m = minimumElevationInMeters) %>% 
+  rename(elev_m = minimumElevationInMeters,
+         datum = geodeticDatum) %>% 
   select(-verbatimElevation) %>% 
   filter(!DOY == 1) #Remove obs where MM and DD = 01 (none of these are accurate)
   
@@ -59,21 +62,66 @@ nemo_2 <- nemo_2 %>%
 
 
 
-#Check for duplicates
-
-#which(duplicated(nemo_2[,c(12,17)]))
-
-#SEE cch1_cleaning.R!!! Remove dupes after combining both 
-
-
 
 #
 # Final cleaning, organizing, and export
 
 nemo_cch2_clean <- nemo_2 %>% 
-  select(-c(coll_number, eventDate, startDayOfYear)) %>% 
+  select(-c(coll_number, eventDate, startDayOfYear, references, datum)) %>% 
   rename(repro = reproductiveCondition, lat = decimalLatitude, long = decimalLongitude, error_dist_m = coordinateUncertaintyInMeters) %>% #Get col names to match CCH1
   select(specimen_number, id, date_new, year, month, day, DOY, lat, long, elev_m, error_dist_m, sub_sp, everything()) #Rearrange df with desired columns in order
 
 write_csv(nemo_cch2_clean, here::here("data_cleaning", "CCH2_scripts_data", "nemo_cch2_cleaned.csv"))
+
+
+
+
+#
+####
+# UPDATED CLEANED CCH2 DATA
+####
+#
+
+
+
+#Adding back the geodetic datum and URLs
+
+nemo_2.5 <- nemo_2 %>% 
+  select(-c(coll_number, eventDate, startDayOfYear)) %>% 
+  rename(repro = reproductiveCondition, lat = decimalLatitude, long = decimalLongitude, error_dist_m = coordinateUncertaintyInMeters) %>% 
+  select(specimen_number, id, date_new, year, month, day, DOY, lat, long, elev_m, error_dist_m, sub_sp, repro, county, locality, habitat, datum, everything()) %>% 
+  mutate(datum = replace(datum, which(datum %in% c("WGS84, Google Earth", "WGS 84")), "WGS84"))
+
+#1452 Obs
+
+#Removing Duplicate Records (Same Lat/Long, Date, [and locality when lat/long = NA])
+library(janitor)
+
+length(which(duplicated(nemo_2.5[,c(3,8,9)]))) #roughly 129 duplicates
+
+nemo_drm <- nemo_2.5 %>% 
+  distinct(date_new, lat, long, sub_sp, locality, county, .keep_all = TRUE) #Retains unique rows for these variables
+#specified sub_sp & locality to hand-pick out duplicates for missing lat/long
+#1409 oobs - Only 43 observations removed
+
+#Hand-check duplicates
+nemo_2_dupes <- get_dupes(nemo_drm, date_new, lat, long, county) %>% 
+  select(specimen_number, everything()) #ID w/ specimen number first
+
+numbers_to_rm <- c("UC24241", "UC24224", "UC63798", "JEPS1894", "JEPS1891", "JEPS1915", "UC63802", "UC24242", "UC131446", "UC126906", "CAS-DS-135657", "UC163963", "JEPS3116", "UC489037", "JEPS1884", "UC572488", "JEPS21152", "UC1069172", "JEPS21153", "UC1069177", "UC596368", "JEPS1910", "UC606907", "UC606909", "SBBG164523", "JEPS1489", "UC606911", "UC606914", "UC666593", "SD00010669", "SD00010496", "SDSU06118", "UCSB030048", "JEPS8961", "SD00010483", "OBI106054", "SDSU06094", "SDSU06112", "UCR0077183", "SBBG164524", "OBI106029", "HSC201536", "OBI106027", "OBI106040", "JEPS82018", "JEPS82020", "UCR0077136", "SDSU15873", "UCR0077135", "IRVC100598", "IRVC100601", "UCR0077132", "OBI106018", "UCR0077114", "UCR0077113", "UCR0077203", "SBBG164511", "UCR0077198", "UCR0077047", "UCR0077195") #Stop line 60
+
+# SDSU06118 removec but sub_sp was different from duplicate (menziesii rm)
+#	OBI106029 and SBBG164524 "in organic garden - removed
+
+nemo_3 <- nemo_drm %>% 
+  filter(!(specimen_number %in% numbers_to_rm)) #bingo. 1676 obs after dupe removal
+#60 more removed for 1349 obs == total of 103 duplicates removed!!
+
+#double check
+#dupes_check2 <- get_dupes(nemo_3, date_new, lat, long, county) %>% 
+ # select(specimen_number, everything()) #ID w/ specimen number first
+# All Good!
+
+write_csv(nemo_3, here::here("data_cleaning", "CCH2_scripts_data", "nemo_cch2_cleaned_V2.csv"))
+
 
--- data_cleaning/CCH2_scripts_data/split_by_herb.R
+++ data_cleaning/CCH2_scripts_data/split_by_herb.R
@@ -12,6 +12,9 @@ library(here)
 
 # Load in the cleaned data - output from cch2_cleaning.R
 
+## Added chunk at bottom to split up V2 of cleaned CCH2 data
+# USE V2 DATA (Cleaned & dupes remnoved)
+
 nemo_cch2_cleaned <- read_csv(here::here("data_cleaning", "CCH2_scripts_data", "nemo_cch2_cleaned.csv"))
 # Looks good
 
@@ -69,3 +72,74 @@ cch2_UCSB <- nemo_cch2_cleaned %>%
   arrange(lat)
 write_csv(cch2_UCSB, here("data_cleaning", "CCH2_scripts_data", "cch2_cleaned_splits", "nemo_cch2_UCSB.csv"), na = "")
 
+
+
+
+
+
+##############################
+## nemo_cch2_cleaned_V2
+##############################
+
+
+
+
+
+
+
+nemo_cch2_cleaned2 <- read_csv(here::here("data_cleaning", "CCH2_scripts_data", "nemo_cch2_cleaned_V2.csv"))
+
+
+
+cch2_AHUC_to_CSUS_V2 <- nemo_cch2_cleaned2 %>% 
+  filter(str_detect(specimen_number, "^AHUC|BSCA|CAS|COLL|CSUS")) %>% 
+  mutate(georef_by = NA,
+         georef_notes = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
+  arrange(lat)
+write_csv(cch2_AHUC_to_CSUS_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_A-C_V2.csv"), na = "")
+
+
+cch2_DAV_to_LOB_V2 <- nemo_cch2_cleaned2 %>% 
+  filter(str_detect(specimen_number, "DAV|HSC|IRVC|JEPS|LOB")) %>% 
+  mutate(georef_by = NA,
+         georef_notes = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
+  arrange(lat)
+write_csv(cch2_DAV_to_LOB_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_D-L_V2.csv"), na = "")
+
+
+cch2_OBI_to_SDSU_V2 <- nemo_cch2_cleaned2 %>% 
+  filter(str_detect(specimen_number, "OBI|RSA|SBBG|SD00|SDSU")) %>% 
+  mutate(georef_by = NA,
+         georef_notes = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
+  arrange(lat)
+write_csv(cch2_OBI_to_SDSU_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_O-SD_V2.csv"), na = "")
+
+
+cch2_SFV_to_UC9_V2 <- nemo_cch2_cleaned2 %>% 
+  filter(str_detect(specimen_number, "SFV|^UC1|^UC2|^UC3|^UC4|^UC5|^UC6|^UC7|^UC8|^UC9")) %>% 
+  mutate(georef_by = NA,
+         georef_notes = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
+  arrange(lat)
+write_csv(cch2_SFV_to_UC9_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_SF-UC_V2.csv"), na = "")
+
+
+cch2_UCR_V2 <- nemo_cch2_cleaned2 %>% 
+  filter(str_detect(specimen_number, "UCR0")) %>% 
+  mutate(georef_by = NA,
+         georef_notes = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
+  arrange(lat)
+write_csv(cch2_UCR_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_UCR_V2.csv"), na = "")
+
+
+cch2_UCSB_V2 <- nemo_cch2_cleaned2 %>% 
+  filter(str_detect(specimen_number, "UCSB")) %>% 
+  mutate(georef_by = NA,
+         georef_notes = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
+  arrange(lat)
+write_csv(cch2_UCSB_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_UCSB_V2.csv"), na = "")
