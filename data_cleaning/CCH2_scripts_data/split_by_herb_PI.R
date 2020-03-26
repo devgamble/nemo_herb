5bfcb4e27248d6249ee275376841dc29fa04a5ea
devgamble <dgamble@ucsb.edu>
2020-03-25 19:58:22 -0700

    Added fully georeferenced files; modifeid image_dl2 to download images by blocks

--- data_cleaning/CCH2_scripts_data/split_by_herb_PI.R
+++ data_cleaning/CCH2_scripts_data/split_by_herb_PI.R
@@ -18,71 +18,151 @@ library(here)
 
 ##Filter out observations that will not be included
 
+#Block A-C
+
 nemo_cch2_AC_g <- 
-  read_csv(here::here("data_cleaning", "CCH2_scripts_data", "georef_splits", "nemo_cch2_A-C_V2_test.csv"))
+  read_csv(here::here("data_cleaning", "CCH2_scripts_data", "georef_splits", "nemo_cch2_A-C_V2.csv"))
 
 nemo_cch2_AC_p <- nemo_cch2_AC_g %>% 
   filter(!(remove_obs %in% "y")) %>% 
-  mutate(plant = NA,
+  mutate(scored_by = NA,
+         notes = NA,
+         plant = NA,
          Total = NA,
          buds_1 = NA,
          flowers_2 = NA,
          spent_3 = NA,
          immature_4 = NA,
-         mature_5 = NA,
-         scored_by = NA,
-         notes = NA) %>% 
+         mature_5 = NA) %>% 
   select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, references, source, everything()) %>% 
+  mutate(datum = case_when(georef_by != '' | datum == "WGS84" ~ "WGS84",
+                           datum == "NAD83" | datum == "NAD 83" ~ "NAD83",
+                           datum == "NAD27" ~ "NAD27")) %>%  #Adds WGS84 datum for all records where GEOLocate was used
   select(c(-remove_obs))
 
 write_csv(nemo_cch2_AC_p, here("data_cleaning", "CCH2_scripts_data", "splits_for_PI", "nemo_cch2_AC_p.csv"), na = "")
 
-#example: 31 observations --> 27
+#Saves updated csv (obs removed) to 'splits_for_PI' folder
+
+#Block D-L
+
+nemo_cch2_DL_g <- 
+  read_csv(here::here("data_cleaning", "CCH2_scripts_data", "georef_splits", "nemo_cch2_D-L_V2.csv"))
+
+nemo_cch2_DL_p <- nemo_cch2_DL_g %>% 
+  filter(!(remove_obs %in% "y")) %>% 
+  mutate(scored_by = NA,
+         notes = NA,
+         plant = NA,
+         Total = NA,
+         buds_1 = NA,
+         flowers_2 = NA,
+         spent_3 = NA,
+         immature_4 = NA,
+         mature_5 = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, references, source, everything()) %>% 
+  mutate(datum = case_when(georef_by != '' | datum == "WGS84" ~ "WGS84",
+                           datum == "NAD83" | datum == "NAD 83" ~ "NAD83",
+                           datum == "NAD27" ~ "NAD27")) %>%  
+  select(c(-remove_obs))
+
+write_csv(nemo_cch2_DL_p, here("data_cleaning", "CCH2_scripts_data", "splits_for_PI", "nemo_cch2_DL_p.csv"), na = "")
 
 
-cch2_DAV_to_LOB_V2 <- nemo_cch2_cleaned2 %>% 
-  filter(str_detect(specimen_number, "DAV|HSC|IRVC|JEPS|LOB")) %>% 
-  mutate(georef_by = NA,
-         georef_notes = NA) %>% 
-  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
-  arrange(lat)
-write_csv(cch2_DAV_to_LOB_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_D-L_V2.csv"), na = "")
+#Block O-SD
+
+nemo_cch2_OS_g <- 
+  read_csv(here::here("data_cleaning", "CCH2_scripts_data", "georef_splits", "nemo_cch2_O-SD_V2.csv"))
+
+nemo_cch2_OS_p <- nemo_cch2_OS_g %>% 
+  filter(!(remove_obs %in% "y")) %>% 
+  mutate(scored_by = NA,
+         notes = NA,
+         plant = NA,
+         Total = NA,
+         buds_1 = NA,
+         flowers_2 = NA,
+         spent_3 = NA,
+         immature_4 = NA,
+         mature_5 = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, references, source, everything()) %>% 
+  mutate(datum = case_when(georef_by != '' | datum == "WGS84" ~ "WGS84",
+                           datum == "NAD83" | datum == "NAD 83" ~ "NAD83",
+                           datum == "NAD27" ~ "NAD27")) %>%  
+  select(c(-remove_obs))
+
+write_csv(nemo_cch2_OS_p, here("data_cleaning", "CCH2_scripts_data", "splits_for_PI", "nemo_cch2_OS_p.csv"), na = "")
 
 
-cch2_OBI_to_SDSU_V2 <- nemo_cch2_cleaned2 %>% 
-  filter(str_detect(specimen_number, "OBI|RSA|SBBG|SD00|SDSU")) %>% 
-  mutate(georef_by = NA,
-         georef_notes = NA) %>% 
-  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
-  arrange(lat)
-write_csv(cch2_OBI_to_SDSU_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_O-SD_V2.csv"), na = "")
+#Block SF-UC
+
+nemo_cch2_SU_g <- 
+  read_csv(here::here("data_cleaning", "CCH2_scripts_data", "georef_splits", "nemo_cch2_SF-UC_V2.csv"))
+
+nemo_cch2_SU_p <- nemo_cch2_SU_g %>% 
+  filter(!(remove_obs %in% "y")) %>% 
+  mutate(scored_by = NA,
+         notes = NA,
+         plant = NA,
+         Total = NA,
+         buds_1 = NA,
+         flowers_2 = NA,
+         spent_3 = NA,
+         immature_4 = NA,
+         mature_5 = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, references, source, everything()) %>% 
+  mutate(datum = case_when(georef_by != '' | datum == "WGS84" ~ "WGS84",
+                           datum == "NAD83" | datum == "NAD 83" ~ "NAD83",
+                           datum == "NAD27" ~ "NAD27")) %>%  
+  select(c(-remove_obs))
+
+write_csv(nemo_cch2_SU_p, here("data_cleaning", "CCH2_scripts_data", "splits_for_PI", "nemo_cch2_SU_p.csv"), na = "")
+
+#Block UCR
+
+nemo_cch2_UCR_g <- 
+  read_csv(here::here("data_cleaning", "CCH2_scripts_data", "georef_splits", "nemo_cch2_UCR_V2.csv"))
+
+nemo_cch2_UCR_p <- nemo_cch2_UCR_g %>% 
+  mutate(scored_by = NA,
+         notes = NA,
+         plant = NA,
+         Total = NA,
+         buds_1 = NA,
+         flowers_2 = NA,
+         spent_3 = NA,
+         immature_4 = NA,
+         mature_5 = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, references, source, everything()) %>% 
+  mutate(datum = case_when(georef_by != '' | datum == "WGS84" ~ "WGS84",
+                           datum == "NAD83" | datum == "NAD 83" ~ "NAD83",
+                           datum == "NAD27" ~ "NAD27"))
+
+write_csv(nemo_cch2_UCR_p, here("data_cleaning", "CCH2_scripts_data", "splits_for_PI", "nemo_cch2_UCR_p.csv"), na = "")
 
 
-cch2_SFV_to_UC9_V2 <- nemo_cch2_cleaned2 %>% 
-  filter(str_detect(specimen_number, "SFV|^UC1|^UC2|^UC3|^UC4|^UC5|^UC6|^UC7|^UC8|^UC9")) %>% 
-  mutate(georef_by = NA,
-         georef_notes = NA) %>% 
-  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
-  arrange(lat)
-write_csv(cch2_SFV_to_UC9_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_SF-UC_V2.csv"), na = "")
+#Block UCSB
 
+nemo_cch2_UCSB_g <- 
+  read_csv(here::here("data_cleaning", "CCH2_scripts_data", "georef_splits", "nemo_cch2_UCSB_V2.csv"))
 
-cch2_UCR_V2 <- nemo_cch2_cleaned2 %>% 
-  filter(str_detect(specimen_number, "UCR0")) %>% 
-  mutate(georef_by = NA,
-         georef_notes = NA) %>% 
-  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
-  arrange(lat)
-write_csv(cch2_UCR_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_UCR_V2.csv"), na = "")
+nemo_cch2_UCSB_p <- nemo_cch2_UCSB_g %>% 
+  filter(!(remove_obs %in% "y")) %>% 
+  mutate(scored_by = NA,
+         notes = NA,
+         plant = NA,
+         Total = NA,
+         buds_1 = NA,
+         flowers_2 = NA,
+         spent_3 = NA,
+         immature_4 = NA,
+         mature_5 = NA) %>% 
+  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, references, source, everything()) %>% 
+  mutate(datum = case_when(georef_by != '' | datum == "WGS84" ~ "WGS84",
+                           datum == "NAD83" | datum == "NAD 83" ~ "NAD83",
+                           datum == "NAD27" ~ "NAD27")) %>%  
+  select(c(-remove_obs))
 
-
-cch2_UCSB_V2 <- nemo_cch2_cleaned2 %>% 
-  filter(str_detect(specimen_number, "UCSB")) %>% 
-  mutate(georef_by = NA,
-         georef_notes = NA) %>% 
-  select(specimen_number:DOY, sub_sp, repro, county:habitat, elev_m, lat:long, error_dist_m, georef_by, georef_notes, datum, source, references) %>% 
-  arrange(lat)
-write_csv(cch2_UCSB_V2, here("data_cleaning", "CCH2_scripts_data", "cleaned_splits_V2", "nemo_cch2_UCSB_V2.csv"), na = "")
-
+write_csv(nemo_cch2_UCSB_p, here("data_cleaning", "CCH2_scripts_data", "splits_for_PI", "nemo_cch2_UCSB_p.csv"), na = "")
 
 
